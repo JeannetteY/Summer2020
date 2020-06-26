3b07a9da6d311871272490fb9458aab42d791e0f
JeannetteY <yangj24@greenhill.org>
2020-06-26 12:18:32 -0500

    New

--- src/com/company/FinalProject.java
+++ src/com/company/FinalProject.java
@@ -31,10 +31,19 @@ search the file for input using if statement
     then and another if statement for if the word is wrong
 return the score
 
-Number Method
+integer Method
 Print line "guess something a number between 1 and 500"
 copy the code from the Word method
 change the println to work for number game
+change strings to Int
+add consume trash string
+
+double Method
+Print line "guess something a number between 1 and 500"
+copy the code from the Word method
+change the println to work for number game
+change strings to double
+add consume trash string
 
 Welcome method
 this is just a void method to fulfill the requirements
@@ -48,28 +57,31 @@ import java.io.FileNotFoundException;
 public class FinalProject {
     public static int count = 0;//the number of times the player plays
     public static void main(String[] args) throws FileNotFoundException {
-        String answer = "";
+        String answer;
         int score = 0;//number of times the player wins
         String reply;
-        File file = new File("C:\\Users\\jeann\\IdeaProjects\\ComputerScience2020\\src\\com\\company\\word game");
-        Scanner input = new Scanner(file);//file reading scanner
         Scanner userInput = new Scanner(System.in); //create Scanner userInput for non file related input
 
         welcome(userInput);//welcome screen void method
 
         do {
-            System.out.println("Would you like to play a word or number game?");
+            File file = new File("C:\\Users\\jeann\\IdeaProjects\\ComputerScience2020\\src\\com\\company\\word game");
+            Scanner input = new Scanner(file);//file reading scanner
+            System.out.println("Would you like to play a word, integer, or double game?");
             reply = userInput.next();
             if (reply.equals("word")) {
                 count++;//adds everytime the player plays
                 score = Word(input, userInput, score);
-
-            } else if (reply.equals("number")){
+            } else if (reply.equals("integer")){
                 count++;
-                score = Number(input, userInput, score);
+                score = integer(input, userInput, score);
+            }else if (reply.equals("double")) {
+                count++;
+                score = Double(input, userInput, score);
             }
             System.out.println("Would you like to guess again?");
             answer = userInput.next();
+            input.close();
         } while (answer.contains("y"));//if the users answer contains y then the whole game runs again
 
         System.out.println("Your score is: " + score+" out of "+ count); //number of wins and games played println
@@ -80,7 +92,7 @@ public class FinalProject {
         System.out.println("Guess a word for something you would find in your home");
         String answerWord = userInput.next();
         while (input.hasNextLine()) {
-            if (input.next().contains(answerWord)) {
+            if (input.next().equals(answerWord)) {
                 flag = true;
                 break;
             } else {
@@ -88,9 +100,10 @@ public class FinalProject {
             }
         }
 
-        if (flag==false) {
+        //this decides if the answer is right or not based on the flag
+        if (!flag) {
             System.out.println("You did not guess correctly");
-        }else if (flag == true){
+        }else if (flag){
             score++;
             System.out.println("Congrats you guessed a correct word!");
         }
@@ -98,22 +111,55 @@ public class FinalProject {
         return score;//returns to the main
     }
 
-    public static int Number(Scanner input, Scanner userInput, int score) { //the method for the numbers game
-        boolean flag = false;
-        System.out.println("You have chosen the numbers game! Guess a number between 0 and 500 and see if you get it right");
-        String answerWord = userInput.next();
+    public static int integer(Scanner input, Scanner userInput, int score) { //the method for the integers game
+        boolean flag = true;
+        System.out.println("You have chosen the Integers game! Guess a integer between 0 and 500 and see if you get it right");
+        int answerInt = userInput.nextInt();
         while (input.hasNextLine()) {
-            if (input.next().contains(answerWord)) {
-                flag = true;
-                break;
+            if (input.hasNextInt()) {
+                if (input.nextInt() == (answerInt)) {
+                    flag = true;
+                    break;
+                } else {
+                    flag = false;
+                }
             } else {
-                flag = false;
+                String trash = input.next();
             }
         }
 
-        if (flag==false) {
+        //this decides if the answer is right or not based on the flag
+        if (!flag) {
             System.out.println("You did not guess correctly");
-        }else if (flag == true){
+        }else if (flag){
+            score++;
+            System.out.println("Congrats you guessed a correct number!");
+        }
+
+        return score;
+    }
+
+    public static int Double(Scanner input, Scanner userInput, int score) { //the method for the doubles game
+        boolean flag = true;
+        System.out.println("You have chosen the double game! Guess a double between 0 and 10 and see if you get it right");
+        double answerDouble = userInput.nextDouble();
+        while (input.hasNextLine()) {
+            if (input.hasNextDouble()) {
+                if (input.nextDouble() == (answerDouble)) {
+                    flag = true;
+                    break;
+                } else {
+                    flag = false;
+                }
+            } else {
+                String trash = input.next();
+            }
+        }
+
+        //this decides if the answer is right or not based on the flag
+        if (!flag) {
+            System.out.println("You did not guess correctly");
+        }else if (flag){
             score++;
             System.out.println("Congrats you guessed a correct number!");
         }
@@ -123,8 +169,23 @@ public class FinalProject {
 
     public static void welcome(Scanner userInput){
         System.out.println("Welcome the Guessing Game!");
-
-        System.out.println("Type start to start");
+        System.out.println("");
+        System.out.println("      ????????????      ");
+        System.out.println("    ????????????????    ");
+        System.out.println("  ???????      ???????  ");
+        System.out.println("  ??????        ??????? ");
+        System.out.println("               ???????? ");
+        System.out.println("              ????????  ");
+        System.out.println("            ????????    ");
+        System.out.println("          ????????      ");
+        System.out.println("         ???????        ");
+        System.out.println("         ???????        ");
+        System.out.println("");
+        System.out.println("         ?????          ");
+        System.out.println("        ???????         ");
+        System.out.println("         ?????          ");
+        System.out.println("");
+        System.out.println("Type start to begin");
         String start = userInput.next();
     }
 }
